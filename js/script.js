3eb6676adae07492ba5c18e38caf6278b532ff18
igtom123 <iangetange123@gmail.com>
2025-03-17 03:53:52 +0300

    changes made half way

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
-const bar = document.getElementById('bar');
-const nav = document .getElementById('navbar');
+const bar = document.getElementById("bar");
+const nav = document.getElementById("navbar");
 
-if(bar) {
-    bar.addEventListener('click',()=> {
-
-        nav.classList.add('ative');
-    })
+if (bar) {
+  bar.addEventListener("click", () => {
+    nav.classList.add("active");
+  });
 }
